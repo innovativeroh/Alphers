6f56cc3ebf8cb4d0f7957e8cf50d644aa78a2c90
Ritik Mitra <ritikmitra36@gmail.com>
2023-12-27 22:08:06 +0530

    nav bar has been designed

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,16 +1,21 @@
 'use-client'
 import React from "react";
 import Styles from "./header.module.css";
-import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { faBell, faBriefcase, faCompass, faHome, faMap, faNavicon, faSearch } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import logo from '../../../public/logo.jpg'
+import Image from "next/image";
+import Link from "next/link";
 
-const Header = () =>{
-    return(
+const Header = () => {
+    return (
         <div className={Styles.header}>
             <div id="wrapper">
                 <div className={Styles.headerStructure}>
                     <div>
-                        Logo
+                        <Link href={"/"}>                    
+                        <Image src={logo} alt="logo" className={Styles.BrandImage}/>
+                        </Link>
                     </div>
                     <div className={Styles.searchArea}>
                         <form action="" >
@@ -18,8 +23,29 @@ const Header = () =>{
                             <FontAwesomeIcon icon={faSearch} className={Styles.searchIcon}></FontAwesomeIcon>
                         </form>
                     </div>
-                    <div>
-                        Navigations
+                    <div >
+                        <nav className={Styles.iconContainer}>
+                            <div>
+                                <FontAwesomeIcon icon={faHome} className={Styles.icon} />
+                            </div>
+                            <div>
+                                <FontAwesomeIcon icon={faCompass} className={Styles.icon} />
+                            </div>
+                            <div>
+                                <FontAwesomeIcon icon={faBriefcase} className={Styles.icon} />
+                            </div>
+                            <div>
+                                <FontAwesomeIcon icon={faBell} className={Styles.icon} />
+                            </div>
+                            <div style={{fontSize : "2em"}}>
+                                |
+                            </div>
+                            <div className={Styles.credentials}>
+                                <Link href={"/login"}>
+                                <button className={Styles.BtnStyle}> Login / SignUp </button>
+                                </Link>
+                            </div>
+                        </nav>
                     </div>
                 </div>
             </div>
