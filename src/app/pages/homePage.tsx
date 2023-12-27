38601a8778991fe0b536f940e0a31d68bcb14dbf
Rohan Sidhu <rohan.sidhu@outlook.com>
2023-12-27 17:21:08 +0530

    Git Push

--- src/app/pages/homePage.tsx
+++ src/app/pages/homePage.tsx
@@ -1,9 +1,13 @@
 import React from "react";
 import Header from "../components/header";
+import Head from "next/head";
 
 const HomePage = () =>{
   return(
     <div>
+      <Head>
+        <title>My page title</title>
+      </Head>
       <Header/>
       <div id="wrapper">
       <h1>Content Here</h1>
